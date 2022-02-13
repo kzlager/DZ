cae4e643da6634940ad45b57afa51382d0eb4acc
kzlager <highcolour@yandex.ru>
2022-02-13 18:12:33 +0300

    adding files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+int GetRandom()
+{
+    int number = new Random().Next(100,1000);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+int result = randomazer % 100 /10;
+Console.WriteLine($"Вторая цифра числа = {result}");
